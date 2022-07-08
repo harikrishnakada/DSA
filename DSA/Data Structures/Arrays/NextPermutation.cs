1ec37ccec849eca17d87a383310c8bc49413a6f3
Hari Krishna <harikrishna.kada@gmail.com>
2022-07-08 20:07:58 +0530

    Optimized the NextPermutation

--- DSA/Data Structures/Arrays/NextPermutation.cs
+++ DSA/Data Structures/Arrays/NextPermutation.cs
@@ -8,92 +8,58 @@ namespace DSA.Data_Structures.Arrays
     {
         public static void Run()
         {
-            var nums = new int[] { 6,2,1,5,4,3,0 };
+            var nums = new int[] { 2, 3, 1, 3, 3, 3 };
 
-            var isAscendingOrder = true;
-            var isDecendingOrder = true;
+            var isTouched = false;
 
             if (nums.Length > 1)
             {
-                for (int i = 0; i < nums.Length; i++)
+                for (int i = nums.Length - 1; i >= 0; i--)
                 {
-                    if (i + 1 < nums.Length)
+                    if (i == 0)
+                        break;
+
+                    if (nums[i] > nums[i - 1])
                     {
-                        if (nums[i] < nums[i + 1])
-                        {
-                            isDecendingOrder = false;
-                        }
-                        if (nums[i] > nums[i + 1])
-                        {
-                            isAscendingOrder = false;
-                        }
+                        isTouched = true;
+                        reverse(nums, i, nums.Length - 1);
 
-                        if(!isAscendingOrder && !isDecendingOrder)
+                        for (int j = i; j < nums.Length; j++)
                         {
-                            break;
-                        }
-                    }
-                }
-                int reversingStartIndex = -1;
-
-                if (isAscendingOrder)
-                {
-                    var pre = nums[nums.Length - 2];
-                    nums[nums.Length - 2] = nums[nums.Length - 1];
-                    nums[nums.Length - 1] = pre;
-                }
-                else if (isDecendingOrder)
-                {
-                    reversingStartIndex = 0;
-                }
-                else
-                {
-                    for (int i = nums.Length - 1; i >= 0; i--)
-                    {
-                        if (i == 0)
-                            break;
-
-                        if (nums[i] > nums[i - 1])
-                        {
-                            int nextLeastGreatestElementIndex = i;
-
-                            for (int j = i + 1; j < nums.Length; j++)
+                            if (nums[j] > nums[i - 1])
                             {
-                                if (j + 1 > nums.Length)
-                                    break;
-
-                                if (nums[j] <= nums[nextLeastGreatestElementIndex] && nums[j] > nums[i - 1])
-                                {
-                                    nextLeastGreatestElementIndex = j;
-                                }
+                                swap(nums, i - 1, j);
+                                break;
                             }
-                            var pre = nums[i - 1];
-                            nums[i - 1] = nums[nextLeastGreatestElementIndex];
-                            nums[nextLeastGreatestElementIndex] = pre;
-
-                            reversingStartIndex = i;
-
-                            break;
                         }
+                        break;
                     }
                 }
 
-                if (reversingStartIndex >= 0)
+                if (!isTouched)
                 {
-                    int temp;
-                    int start = reversingStartIndex;
-                    int end = nums.Length - 1;
-                    while (start < end)
-                    {
-                        temp = nums[start];
-                        nums[start] = nums[end];
-                        nums[end] = temp;
-                        start++;
-                        end--;
-                    }
+                    reverse(nums, 0, nums.Length - 1);
                 }
             }
             Console.WriteLine(String.Join(" ", nums));
+        }
+        public static void reverse(int[] nums, int start, int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        public static void swap(int[] nums, int source, int target)
+        {
+            var pre = nums[source];
+            nums[source] = nums[target];
+            nums[target] = pre;
         }
     }
 }
