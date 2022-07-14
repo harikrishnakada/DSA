cbfa08c4c8bf57d2622b1cb3b992f57e23457516
Hari Krishna <harikrishna.kada@gmail.com>
2022-07-14 13:35:36 +0530

    Find square root of x

---
+++ DSA/Algorithms/BinarySearch/Sqrx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Algorithms.BinarySearch
+{
+    public static class Sqrx
+    {
+        public static int Run()
+        {
+            int num = 2147395599;
+            long l = 1, h = num / 2, m = -1;
+            long ans = -1;
+
+            if (num == 1)
+                return 1;
+
+
+            while (l <= h)
+            {
+                m = l + (h - l) / 2;
+                long sqr = m * m;
+
+                if (num < sqr)
+                {
+                    h = m - 1;
+                }
+                else if (num > sqr)
+                {
+                    ans = m;
+                    l = m + 1;
+                }
+                else
+                {
+                    ans = m;
+                    break;
+                }
+            }
+
+            Console.WriteLine(ans);
+            return Convert.ToInt32(ans);
+        }
+    }
+}
