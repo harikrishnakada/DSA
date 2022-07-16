e1594f1ff2f45b5d9061065a281ee87c0c895562
Hari Krishna <harikrishna.kada@gmail.com>
2022-07-16 19:02:52 +0530

    Valid palindrome

---
+++ DSA/Data Structures/ValidPalindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Data_Structures
+{
+    public class ValidPalindrome
+    {
+        public static void Run()
+        {
+            var s = "atbbga";
+            int l = 0, h = s.Length - 1;
+
+            bool isMatched = true;
+            while (l < h)
+            {
+                if (s[l] != s[h])
+                {
+                    isMatched = CheckPalindrome(s, l + 1, h);
+                    if (!isMatched)
+                        isMatched = CheckPalindrome(s, l, h - 1);
+                    break;
+                }
+                h--;
+                l++;
+            }
+
+            Console.WriteLine(isMatched);
+        }
+
+        public static bool CheckPalindrome(string s, int i, int j)
+        {
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                    return false;
+
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
