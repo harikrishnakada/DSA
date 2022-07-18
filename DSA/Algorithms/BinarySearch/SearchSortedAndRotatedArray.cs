9f7a95f146a5ce1a46fe6e03c34e54e2bd8088c7
Hari Krishna <harikrishna.kada@gmail.com>
2022-07-18 06:07:41 +0530

    Search Sorted and rotated array.

---
+++ DSA/Algorithms/BinarySearch/SearchSortedAndRotatedArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Algorithms.BinarySearch
+{
+    public class SearchSortedAndRotatedArray
+    {
+        public static int Run()
+        {
+            var nums = new int[] { 3, 1 };
+            int target = 1;
+            int res = -1;
+
+            int l = 0, h = nums.Length - 1, m = -1;
+
+            while (l <= h)
+            {
+                m = l + (h - l) / 2;
+
+                if (nums[m] == target)
+                {
+                    res = m;
+                    break;
+                }
+                if (nums[l] <= nums[m])
+                {
+                    if (nums[l] <= target && nums[m] > target)
+                    {
+                        h = m - 1;
+                    }
+                    else
+                    {
+                        l = m + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[m] < target && target <= nums[h])
+                    {
+                        l = m + 1;
+                    }
+                    else
+                    {
+                        h = m - 1;
+                    }
+                }
+            }
+            return res;
+        }
+
+    }
+}
