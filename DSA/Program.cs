e36fb9ceb5d2e976df8a3c43e31c3c4d64e45299
Hari Krishna <harikrishna.kada@gmail.com>
2022-09-28 19:45:58 +0530

    Swapper and reverse

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+
+using DSA.Algorithms.BinarySearch;
+using DSA.Data_Structures;
+using DSA.Data_Structures.Arrays;
 
 namespace DSA
 {
@@ -11,38 +16,65 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            //NewMethod();
-            SumAsPerFrequency();
         }
-        private static void SumAsPerFrequency()
+
+        public static void swapper(int[] arr, int startIndex)
         {
-            var n = 8;
-            var arr = new int[] { 4, 4, 6, 5, 3, 3, 3, 9 };
-
-            var testCases = new List<IList<int>>();
-            testCases.Add(new int[] { 1, 4 });
-            testCases.Add(new int[] { 2, 7 });
-            testCases.Add(new int[] { 3, 7 });
-            testCases.Add(new int[] { 5, 6 });
-
-            var Q = testCases.Count;
-
-            foreach (var item in testCases)
+            int tempI = 0;
+            while (startIndex < arr.Length)
             {
-                var arrIx = 0;
-                var sum = 0;
-                while (arrIx < arr.Length)
-                {
-                    if (arr[arrIx] >= item[0] && arr[arrIx] <= item[1])
-                        sum += arr[arrIx];
+                int temp = arr[tempI];
+                arr[tempI] = arr[startIndex];
+                arr[startIndex] = temp;
 
-                    arrIx++;
-                }
-
-                Console.WriteLine(sum);
+                tempI++;
+                startIndex++;
             }
+        }
+
+        public static void reverse(int[] nums, int start, int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        public static void Run()
+        {
+            int[] arr = new int[] { -1, -3, 0, 2 };
+            int num = 0;
+            arr = arr.OrderBy(x => x).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] >= 0)
+                {
+                    if (i == arr.Length - 1)
+                    {
+                        num = arr[i] + 1;
+                        break;
+                    }
+
+                    if (arr[i + 1] - arr[i] > 1)
+                    {
+                        num = arr[i] + 1;
+                        break;
+                    }
+                }
+            }
+            if (num == 0)
+                num++;
+            Console.WriteLine(num);
+
+
         }
 
         private static void Spaces()
